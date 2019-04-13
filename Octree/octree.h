efcf4c094946938f73903cde51b8998fcacac67e
Adriana Perez <adriana.perez@ucsp.edu.pe>
2019-04-12 20:53:46 -0500

    Update octree.h

--- Octree/octree.h
+++ Octree/octree.h
@@ -6,6 +6,7 @@
 #include "OctreePoint.h"
 
 #include <iostream>
+int max_points = 4;
 
 using namespace std;
 int cont = 0;
@@ -17,8 +18,8 @@ public:
 	
 
 	Octree *children[8];
-	vector<OctreePoint> *data;
-	vector<OctreePoint> dat;
+	OctreePoint *data;
+	vector<OctreePoint*> points;
 
 	/*
 			hijos:	0 1 2 3 4 5 6 7
@@ -55,7 +56,14 @@ public:
 
 	bool isLeafNode() const { //si no tenemos hijo return 1, osea estamos vacios en ese octante
 		return children[0] == NULL;
-	
+		/*int cont2 = 0;
+		for (int i = 0; i < 8; ++i)
+			if (children[i] != NULL)
+				cont2++;//return false;
+
+		if (cont2 >= cont)
+			return true;
+		return false;*/
 	}
 
 
@@ -64,7 +72,7 @@ public:
 		if (isLeafNode()) { 
 			if (data == NULL) { //si estamos vacios pongo el punto y ya, sin partir
 				data = point;
-				
+				points.push_back(data);
 			//	cout << "Octante en el que esta el punto:  " << point->getPosition().D[0] << point->getPosition().D[1] << point->getPosition().D[2] << " : " << getOctant(point->getPosition()) << endl;
 				return;
 			}
@@ -72,29 +80,33 @@ public:
 
 			else { //si tiene un punto entonces parto y agrego 8 hijos
 				// y inserto la data antigua con la nueva (reacomodar)
-				cout << "SE DIVIDE " << endl;
-				cout << "Nueva dimension: " << ((halfDimension *.5f).D[0]) * 2<< ", Origen: " << (halfDimension * .5f).D[0]<<endl;
-				cout << "Octante en el que esta el punto:  " << point->getPosition().D[0] << ", " << point->getPosition().D[1] << ", "<<point->getPosition().D[2] << " : " << getOctant(point->getPosition()) << endl;
-				cont++;
-				// guardar anterior punto
-				OctreePoint *oldPoint = data;
-				data = NULL;
+				if (points.size() >= max_points) { // puntos por cuadrante
+					cout << "SE DIVIDE " << endl;
+					cout << "Nueva dimension: " << ((halfDimension *.5f).D[0]) * 2 << ", Origen: " << (halfDimension * .5f).D[0] << endl;
+					cout << "Octante en el que esta el punto:  " << point->getPosition().D[0] << ", " << point->getPosition().D[1] << ", " << point->getPosition().D[2] << " : " << getOctant(point->getPosition()) << endl;
+						cont++;
+						// guardar anterior punto
+					OctreePoint *oldPoint = data;
+					data = NULL;
 
-				// dividir el nodo actual en 8
-				for (int i = 0; i < 8; ++i) {
-					// calcular nuevo cubito 
-					Vec3 newOrigin = origin;
-					newOrigin.x += halfDimension.x * (i & 4 ? .5f : -.5f); // si es + o - dividir 
-					newOrigin.y += halfDimension.y * (i & 2 ? .5f : -.5f);
-					newOrigin.z += halfDimension.z * (i & 1 ? .5f : -.5f);
-					children[i] = new Octree(newOrigin, halfDimension*.5f); /// el origen y las nuevas dim de nuevo cubo, aqui se crea practicamente los cubos
-					//cout << newOrigin.D[0] << " " << newOrigin.D[0] << " " << newOrigin.D[0] << " half dim: " << (halfDimension *.5f).D[0];
+					// dividir el nodo actual en 8
+					for (int i = 0; i < 8; ++i) {
+						// calcular nuevo cubito 
+						Vec3 newOrigin = origin;
+						newOrigin.x += halfDimension.x * (i & 4 ? .5f : -.5f); // si es + o - dividir 
+						newOrigin.y += halfDimension.y * (i & 2 ? .5f : -.5f);
+						newOrigin.z += halfDimension.z * (i & 1 ? .5f : -.5f);
+						children[i] = new Octree(newOrigin, halfDimension*.5f); /// el origen y las nuevas dim de nuevo cubo, aqui se crea practicamente los cubos
+						//cout << newOrigin.D[0] << " " << newOrigin.D[0] << " " << newOrigin.D[0] << " half dim: " << (halfDimension *.5f).D[0];
+					}
+
+
+					// parte recursiva donde reorganizamos los puntos del mismo octante
+					children[getOctant(oldPoint->getPosition())]->insert(oldPoint, cont);
+					children[getOctant(point->getPosition())]->insert(point, cont);
 				}
-			
-
-				// parte recursiva donde reorganizamos los puntos del mismo octante
-				children[getOctant(oldPoint->getPosition())]->insert(oldPoint, cont);
-				children[getOctant(point->getPosition())]->insert(point,  cont);
+				else
+					points.push_back(data);
 			}
 		}
 		else {
